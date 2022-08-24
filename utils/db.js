a824babf933122898d1155d3cdde8e62d36d6b89
Esoteric918 <3411@holbertonschool.com>
2022-08-24 15:08:04 -0500

    task 1 add db file and main tst file

---
+++ utils/db.js
@@ -0,0 +1,38 @@
+const { MongoClient } = require('mongodb');
+
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const database = process.env.DB_NAME || 'file_manager';
+
+const url = `mongodb://${host}:${port}/${database}`;
+
+class DBClient {
+  constructor() {
+    this.isAlive = function isAlive()  { return false; }
+
+    MongoClient.connect(url,
+      { useNewUrlParser: true,  useUnfiedTopology: true },
+      (err, client) => {
+        if (err) {
+          // client.close();
+          console.log(err);
+        } else {
+          this.db = client.db(database);
+          this.users = this.db.collection('users');
+          this.files = this.db.collection('files');
+          console.log('MongoDB connected');
+          this.isAlive = function isAlive()  { return true; }
+        }
+      });
+  }
+  async nbUsers() {
+    return this.users.countDocuments();
+  }
+
+  async nbFiles() {
+    return this.files.countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
