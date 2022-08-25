b71e985c9299256d682444e2b342fbd02391b38e
Esoteric918 <3411@holbertonschool.com>
2022-08-25 14:49:29 -0500

    task4

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,33 +1,48 @@
 const sha1 = require('sha1');
-const mon = require('mongodb');
+const mongo = require('mongodb');
 const Redis = require('../utils/redis');
 const dbClient = require('../utils/db');
 
 class UsersController {
-  static async createUser(req, res) {
-    const { email, password } = req.body;
+  static postNew(req, res) {
+    (async () => {
+      const { email, password } = req.body;
 
-    if (!email) return res.status(400).send({ error: 'Missing email' });
-    if (!password) return res.status(400).send({ error: 'Missing password' });
-    if (await dbClient.users.findOne({ email })) return res.status(400).send({ error: 'Already exist' });
-    const newUser = await dbClient.users.insertOne({
-      email,
-      password: sha1(password),
-    });
-    return res.status(201).send({ id: newUser.insertedId, email });
+      if (!email) {
+        return res.status(400).json({ error: 'Missing email' });
+      }
+
+      if (!password) {
+        return res.status(400).json({ error: 'Missing password' });
+      }
+
+      const user = await dbClient.db.collection('users').findOne({ email });
+      if (user) {
+        return res.status(400).json({ error: 'Already exist' });
+      }
+
+      const hash = sha1(password);
+      const newUser = await dbClient.db
+        .collection('users')
+        .insertOne({ email, password: hash });
+      return res.status(201).send({ id: newUser.insertedId, email });
+    })();
   }
 
   static getMe(req, res) {
     (async () => {
-      const authtoken = req.headers['x-token'];
-      const user = await Redis.get(`auth_${authtoken}`);
-      const userInfo = await dbClient.db
+      const token = req.headers['x-token'];
+      const user = await Redis.get(`auth_${token}`);
+      const userID = new mongo.ObjectID(user);
+      const userData = await dbClient.db
         .collection('users')
-        .findOne({ _id: new mon.ObjectID(user) });
-      if (!userInfo) {
-        return res.status(401).json({ error: 'Unauthorized' });
+        .findOne({ _id: userID });
+
+      if (!userData) {
+        return res.status(401).send({ error: 'Unauthorized' });
       }
-      return res.status(200).json({ id: userData._id, email: userData.emailr });
+
+      return res.status(200).send({ id: userData._id, email: userData.email });
     })();
   }
 }
