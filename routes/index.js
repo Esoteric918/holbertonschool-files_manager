d2be47a1883884f14967ed46a6af3f9ab603ee60
Esoteric918 <3411@holbertonschool.com>
2022-08-24 21:40:44 -0500

    linter fix

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const AppCon = require('../controllers/AppController');
 // const UserCon = require('../controllers/UserController');
 
-
 const router = express.Router();
 
 router.get('/status', AppCon.getStatus);
