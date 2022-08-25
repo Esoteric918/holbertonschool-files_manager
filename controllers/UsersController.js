dac77ebfafa078c796ae4ad996ec38d16b10efd6
Esoteric918 <3411@holbertonschool.com>
2022-08-25 15:16:54 -0500

    revert

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,58 +1,34 @@
 const sha1 = require('sha1');
-const mongo = require('mongodb');
+const mon = require('mongodb');
 const Redis = require('../utils/redis');
 const dbClient = require('../utils/db');
 
 class UsersController {
-  static postNew(req, res) {
-    (async () => {
-      const { email, password } = req.body;
+  static async createUser(req, res) {
+    const { email, password } = req.body;
 
-      if (!email) {
-        return res.status(400).json({ error: 'Missing email' });
-      }
-
-      if (!password) {
-        return res.status(400).json({ error: 'Missing password' });
-      }
-
-      const user = await dbClient.db.collection('users').findOne({ email });
-      if (user) {
-        return res.status(400).json({ error: 'Already exist' });
-      }
-
-      const hash = sha1(password);
-      const newUser = await dbClient.db
-        .collection('users')
-        .insertOne({ email, password: hash });
-      return res.status(201).send({ id: newUser.insertedId, email });
-    })();
+    if (!email) return res.status(400).send({ error: 'Missing email' });
+    if (!password) return res.status(400).send({ error: 'Missing password' });
+    if (await dbClient.users.findOne({ email })) return res.status(400).send({ error: 'Already exist' });
+    const newUser = await dbClient.users.insertOne({
+      email,
+      password: sha1(password),
+    });
+    return res.status(201).send({ id: newUser.insertedId, email });
   }
 
-<<<<<<< HEAD
   static getMe(req, res) {
     (async () => {
-      const token = req.headers['x-token'];
-      const user = await Redis.get(`auth_${token}`);
-      const userID = new mongo.ObjectID(user);
-      const userData = await dbClient.db
+      const authtoken = req.headers['x-token'];
+      const user = await Redis.get(`auth_${authtoken}`);
+      const userInfo = await dbClient.db
         .collection('users')
-        .findOne({ _id: userID });
-
-      if (!userData) {
-        return res.status(401).send({ error: 'Unauthorized' });
+        .findOne({ _id: new mon.ObjectID(user) });
+      if (!userInfo) {
+        return res.status(401).json({ error: 'Unauthorized' });
       }
-
-      return res.status(200).send({ id: userData._id, email: userData.email });
+      return res.status(200).json({ id: userData._id, email: userData.emailr });
     })();
-=======
-  static async getMe(req, res) {
-    const authToken = `auth_${req.headers['x-token']}`;
-    const userId = await Redis.get(authToken);
-    if (!userId) return res.status(401).json({ error: 'Unauthorized' });
-    const user = await dbClient.users.findOne({ _id: new mon.ObjectId(userId) });
-    return res.json({ id: user._id, email: user.email });
->>>>>>> parent of a6d7e832... task4
   }
 }
 
