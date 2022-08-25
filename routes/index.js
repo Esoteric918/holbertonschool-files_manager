80960f9eab4e6a531ad33b2c7e0e3cab340a0b6f
Esoteric918 <3411@holbertonschool.com>
2022-08-24 21:56:53 -0500

    task 3

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
 const AppCon = require('../controllers/AppController');
-// const UserCon = require('../controllers/UserController');
+const UserCon = require('../controllers/UsersController');
 
 const router = express.Router();
-
+// GET routes
 router.get('/status', AppCon.getStatus);
 router.get('/stats', AppCon.getStats);
+
+// POST routes
+router.post('/users', UserCon.createUser);
 
 module.exports = router;
